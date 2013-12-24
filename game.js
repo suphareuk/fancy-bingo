6bbb4656732da34efdd9ff36c788feb672dc89e7
Toon-BKK <toon@abctech-thailand.com>
2013-12-24 15:51:40 +0700

    update javaScript

--- game.js
+++ game.js
@@ -1,15 +1,21 @@
-var bingoNumbers = [];
+var userBingoNumbers = [],
+	rollNumbers	 	 = [],
+	numberLog		 = [],
+	columnB 		 = [],
+	columnI 		 = [],
+	columnN 		 = [],
+	columnG 		 = [],
+	columnO 		 = [];
 
-function generateBingoNumber() {
+function generateBingoNumber(targetArray) {
 	for (var i = 0; i < 75; i++) {
-		bingoNumbers.push(i+1);		
+		targetArray.push(i+1);		
 	}
 }
 
 function shuffleBingoNumber(theLength, bingNums) {
-
-	var bingoNumberTemp, // A temporary variable to hold reference to index variable i points to
-		bingoNumberSwap; // The index we will swap  (i.e. the random number)
+	var bingoNumberTemp = 0, // A temporary variable to hold reference to index variable i points to
+		bingoNumberSwap = 0; // The index we will swap  (i.e. the random number)
 
 	for (var i = theLength; i > 0; i--) {
 	    bingoNumberSwap = Math.floor(Math.random() * i);
@@ -20,7 +26,36 @@ function shuffleBingoNumber(theLength, bingNums) {
 
 }
 
-generateBingoNumber();
-shuffleBingoNumber(bingoNumbers.length - 1, bingoNumbers);
-console.log(bingoNumbers);
+function rollNumber(targetArray) {
+	var targetArrayLen = targetArray.length - 1;
+	shuffleBingoNumber(targetArrayLen, targetArray);	
+	numberLog.push(targetArray[0]);
+	targetArray.shift();
+}
 
+generateBingoNumber(rollNumbers);
+generateBingoNumber(userBingoNumbers);
+shuffleBingoNumber(userBingoNumbers.length - 1, userBingoNumbers)
+
+for (var i = 0; i < userBingoNumbers.length - 1; i++) {
+	if (userBingoNumbers[i] <= 15) {
+		columnB.push(userBingoNumbers[i]);
+	} else if (userBingoNumbers[i] >= 16 && userBingoNumbers[i] <= 30) {
+		columnI.push(userBingoNumbers[i]);
+	} else if (userBingoNumbers[i] >= 31 && userBingoNumbers[i] <= 45) {
+		columnN.push(userBingoNumbers[i]);
+	} else if (userBingoNumbers[i] >= 46 && userBingoNumbers[i] <= 60) {
+		columnG.push(userBingoNumbers[i]);
+	} else if (userBingoNumbers[i] >= 61 && userBingoNumbers[i] <= 75) {
+		columnO.push(userBingoNumbers[i]);
+	}
+	
+};
+
+// Simulate roll turn
+/*for (var i = 1; i <= 20; i++) {
+	rollNumber(winnerNumbers);
+	console.log('Roll ' + i);
+	console.log("number log!!!!!!!");
+	console.log(numberLog);
+};*/
