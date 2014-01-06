9d982603335e1035d0d5a578f1e4eb5e5de032b7
Boom <boom@abctech-thailand.com>
2014-01-06 11:26:50 +0700

    Fixed login with exist user

--- fancy-bingo/client/fancy-bingo-client.js
+++ fancy-bingo/client/fancy-bingo-client.js
@@ -5,16 +5,31 @@ Template.home.events({
 
         var player_name = document.getElementsByName("player_name")[0].value;
         var players = Players.find({ name: player_name });
+        // if (players.count() == 0) {
+        //     // ok. you can use this name. go to game page.
+        //     var new_player_id = Players.insert({name: player_name});
+        //     Session.set('room','yes');
+        //     Session.set('userid',new_player_id);
+        //     Session.set('username',player_name);
+        //     Router.go('game');
+        // } else {
+        //     error_elm.innerText = "Someone already used this name. Please login with another name!"; 
+        // }
+
+        var new_player_id;
+
         if (players.count() == 0) {
             // ok. you can use this name. go to game page.
-            var new_player_id = Players.insert({name: player_name});
-            Session.set('room','yes');
-            Session.set('userid',new_player_id);
-            Session.set('username',player_name);
-            Router.go('game');
+            new_player_id = Players.insert({name: player_name});
         } else {
-            error_elm.innerText = "Someone already used this name. Please login with another name!"; 
+            //error_elm.innerText = "Someone already used this name. Please login with another name!"; 
+            new_player_id = Players.update(this._id, { $set: {'name': players}});
         }
+
+        Session.set('room','yes');
+        Session.set('userid',new_player_id);
+        Session.set('username',player_name);
+        Router.go('game');
     }
 });
 
